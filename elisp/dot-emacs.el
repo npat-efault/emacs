c00d4d1b3a3f2ee5052a00cb298a27c1f3a6b693
Nick Patavalis <npat@efault.net>
2015-10-24 19:26:29 +0300

    Remove w3m initialization. Chromium default browser

--- elisp/dot-emacs.el
+++ elisp/dot-emacs.el
@@ -45,7 +45,7 @@
 (load-library "dot-greek")
 (load-library "dot-keys")
 (load-library "dot-modes")
-(load-library "dot-w3m")
+;(load-library "dot-w3m")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -57,6 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-chromium))
  '(default-frame-alist (quote ((vertical-scroll-bars) (height . 40) (width . 80))))
  '(global-edit-server-edit-mode t)
  '(initial-frame-alist (quote ((menu-bar-lines . 0) (tool-bar-lines . 0) (width . 80) (height . 40))))
