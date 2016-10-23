bb2ec51adbad159e8085e6102c850ba600f0c25b
Nick Patavalis <npat@efault.net>
2016-10-23 05:58:56 +0300

    Added text-scale bindings

--- elisp/dot-keys.el
+++ elisp/dot-keys.el
@@ -10,3 +10,6 @@
 ;;(global-set-key [C-f4] 'make-frame-command)
 (global-set-key [menu] 'menu-bar-mode)
 (global-set-key [?\C-x ?g] 'magit-status)
+(global-set-key [?\C-=] 'text-scale-increase)
+(global-set-key [?\C-+] 'text-scale-increase)
+(global-set-key [?\C--] 'text-scale-decrease)
